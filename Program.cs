61bd3433fa509d3e2cf836de02e79db6d9a1ba1e
Felipe Schossler <felipealvesschossler@gmail.com>
2019-10-24 07:04:00 -0300

    First commit with only new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchosslerBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
